be068c0d830a1631f6ea435139d24739a7a91c94
aram_sargsyan <aram_sargsyan@edu.aua.am>
2020-11-24 23:16:59 +0400

    add Proxy fix

--- src/app.py
+++ src/app.py
@@ -7,10 +7,12 @@ from src.utils.logger.logger import make_logger
 from src.api.controllers.account_controller import account_controller
 from src.api.controllers.users_controller import users_controller
 from gevent.pywsgi import WSGIServer
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 log = make_logger(__name__)
-api = Api(app, default="AUA Organization Chart")
+app.wsgi_app = ProxyFix(app.wsgi_app)
+api = Api(app)
 
 api.add_namespace(account_controller)
 api.add_namespace(users_controller)
