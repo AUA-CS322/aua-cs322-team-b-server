4c0ee2e58b89a42606f4f7295090f9e50599ce8d
artyom.abrahamyan <artyom_abrahamyan@edu.aua.am>
2020-12-07 15:25:56 +0400

    Merge with master.
    Merge conflicts with master.
    Remove redundant newline.

--- src/data/organization_chart.py
+++ src/data/organization_chart.py
@@ -18,7 +18,7 @@ class OrganizationChart:
         root_node_id = self._chart[0]['parent']
         self._chart.insert(0, {'id': root_node_id, 'parent': None})
         for user_info in self._chart:
-            user, parent_user = self.get_user_with_parent(user_info)
+            user, parent_user = self.get_user_with_parent(user_info['id'], user_info)
             if user is None:
                 raise Exception('The id provided does not exist in data.')
             chart_node = {}
